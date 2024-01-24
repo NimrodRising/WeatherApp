b097c8f81414d34df3ee94e7f529cd840433e907
NimrodRising <seanmoraczewski@gmail.com>
2024-01-23 20:39:22 -0500

    basic layout of app without logic or component separation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import Searchbar from "./components/SearchBar";
 
 function App() {
-  return <Searchbar />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        padding: "100px",
+        fontSize: "35px",
+        fontWeight: "100",
+      }}
+      className="app"
+    >
+      <Searchbar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,57 @@
+import sunny from "../icons/sunny.svg";
+import wind from "../icons/wind.svg";
+import umbrella from "../icons/umbrella.svg";
+import raindrop from "../icons/raindrop.svg";
+import { useState } from "react";
+
 const Searchbar = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <div className="search-bar">
-        The weather in
-        <input className="input" />
-        ...
+        Right now in{" "}
+        <span
+          contentEditable={true}
+          style={{ outline: "none", fontWeight: "400" }}
+        >
+          Content
+        </span>
+        , it is Partly Cloudy.
       </div>
-      <div className="weather-summary">
-        <div className="weather-summary__temp">50°</div>
-        <div className="weather-summary__details">
-          <div>Windspeed</div>
-          <div>Chance of rain</div>
-          <div>Humidity</div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "500px",
+        }}
+        className="weather-summary"
+      >
+        <div className="weather-summary__icon">
+          <img src={sunny} style={{ width: "200px" }} alt="" />
+        </div>
+        <div className="weather-summary__temp" style={{ fontSize: "100px" }}>
+          50°
+        </div>
+        <div
+          className="weather-summary__details"
+          style={{ display: "flex", flexDirection: "column", gap: "5px" }}
+        >
+          <div style={{ display: "flex", gap: "10px" }}>
+            <img src={wind} style={{ width: "40px" }} />6 mph
+          </div>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <img src={umbrella} style={{ width: "40px" }} />0 %
+          </div>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <img src={raindrop} style={{ width: "40px" }} />
+            80%
+          </div>
         </div>
       </div>
     </div>
